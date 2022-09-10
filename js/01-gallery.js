172500605b8d85a73e3e825ed6586224120c5ef5
Килимник Дмитро <dinakilimnik@gmail.com>
2022-09-10 06:50:24 +0300

    all

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const globalDiv = document.querySelector('.gallery');
+for(let galleryItem of galleryItems){
+    globalDiv.insertAdjacentHTML('beforeend',`<div class="gallery__item"><a class="gallery__link" onclick="return false" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}"  alt="${galleryItem.description}" /></a></div>`);
+    console.log(globalDiv);
+}
+globalDiv.addEventListener('click', funk);
+function funk(evt){
+    const dataValue = evt.target.getAttribute('data-source');
+    const instance = basicLightbox.create(`
+	<img class="gallery__item" src="${dataValue}" alt="Картинка не подгружаеться">
+`)
+
+instance.show(() => console.log('lightbox now visible'))
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const globalDiv = document.querySelector('.gallery');
+for(let galleryItem of galleryItems){
+    globalDiv.insertAdjacentHTML('beforeend',`<a class="gallery__link" onclick="return false" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}"  alt="${galleryItem.description}" /></a>`);
+    console.log(globalDiv);
+}
+// const dataValue = evt.target.getAttribute('data-source');
+new SimpleLightbox('.gallery a');
+
+//  gallery.on('show.simplelightbox', function () {
+//      `<img class="gallery__item" src="${dataValue}" alt="Картинка не подгружаеться">`
+//  });
