498e2e3e2a37ed39376719c32d695d35e3d963d9
Килимник Дмитро <dinakilimnik@gmail.com>
2022-10-07 15:31:46 +0300

    50%

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
 
 
 
 const globalDiv = document.querySelector('.gallery');
-for(let galleryItem of galleryItems){
-    globalDiv.insertAdjacentHTML('beforeend',`<div class="gallery__item"><a class="gallery__link" onclick="return false" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}"  alt="${galleryItem.description}" /></a></div>`);
-    console.log(globalDiv);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item"><a class="gallery__link" onclick="return false" href="${original}"> <img class="gallery__image" src="${preview}" data-source="${original}"  alt="${description}" /></a></div>
+    `;
+    })
+    .join("");
 }
-globalDiv.addEventListener('click', funk);
-function funk(evt){
+globalDiv.innerHTML = createGalleryMarkup(galleryItems);
+
+globalDiv.addEventListener('click', (evt) =>{
+  if(evt.target.nodeName == "IMG"){
     const dataValue = evt.target.getAttribute('data-source');
     const instance = basicLightbox.create(`
 	<img class="gallery__item" src="${dataValue}" alt="Картинка не подгружаеться">
 `)
-
 instance.show(() => console.log('lightbox now visible'))
-}
+  }
+
+});
+
+
